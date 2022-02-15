fbc1adcdfae92ef636c5fb5ed9e0344c1ddad64d
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-02-15 17:13:40 +0100

    finalizing

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const arrow = document.querySelector('#arrow');
+const arrow = document.querySelector('.primary');
+const arrowTwo = document.querySelector('.secondary');
 const cardFooter = document.querySelector('.card-footer');
 const mobileFooter = document.querySelector('.footer--mobile');
 
 arrow.addEventListener('click', () => {
-    cardFooter.classList.toggle('is-hidden');
-    mobileFooter.classList.toggle('is-hidden');
+    mobileFooter.classList.remove('is-hidden');
+    cardFooter.classList.add('is-hidden');
+})
+
+arrowTwo.addEventListener('click', () => {
+    cardFooter.classList.remove('is-hidden');
+    mobileFooter.classList.add('is-hidden');
 })
